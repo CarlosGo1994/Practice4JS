bc9cff472baa0677254a4df18c73bdda3d3a3888
CarlosGo1994 <charls21ronald@gmail.com>
2020-02-13 23:45:58 -0600

    What is a constant? Basic example

---
+++ script.js
@@ -0,0 +1,18 @@
+'use strict'
+
+//Constants
+//This a variable but its value never change
+var web ="http://google.com.mx";
+const ip = "192.88.0.12";
+
+web = "https://www.youtube.com/";
+
+//You can't modify the value to the const
+/*ip="22222";*/
+
+console.log(web,ip);
+
+//Example whith typeof
+console.log(typeof web);
+console.log(typeof ip);
+
